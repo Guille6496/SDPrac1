4af15734d41b4129a7086782cc2dab981eaf27d3
Guille6496 <guillem6496@gmail.com>
2017-02-22 13:55:56 +0100

    Update tracker.py

--- tracker.py
+++ tracker.py
@@ -1,19 +1,21 @@
 from pyactor.context import set_context, create_host, sleep, shutdown
 
 class Tracker(object):
-	_ask = ['announce','get_peers']
-	_tell = ['announce','get_peers']
-    peers=[]
+    _ask = ['announce','get_peers']
+    _tell = ['announce','get_peers']
+    peers = {}
     def announce(self,torrent_hash, peer_ref):
-		self.peers[torrent_hash] = peer_ref
+        self.peers[torrent_hash] = peer_ref
 	
-	def get_peers(self,torrent_hash):
-		print self.peers[torrent_hash]
+    def get_peers(self,torrent_hash):
+        print self.peers[torrent_hash]
 		
 if __name__ == "__main__":
     set_context()
     h = create_host()
     tracker = h.spawn('tracker',Tracker)
     tracker.announce(1234,12345)
+    tracker.announce(1235,12354)
     tracker.get_peers(1234)
+    tracker.get_peers(1235)
     shutdown()
